0864b4afde6982de8ab24b13e5b2b90da6b8ab8a
eduleadpro <eduleadpro06@gmail.com>
2026-05-08 13:28:47 +0530

    feat: initialize express serverless handler with CORS and session middleware

--- api/index.ts
+++ api/index.ts
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
   }
 
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, x-user-name");
   res.header("Access-Control-Allow-Credentials", "true");
 
   // Handle preflight requests explicitly
